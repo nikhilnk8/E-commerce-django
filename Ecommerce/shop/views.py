042700269672e9f8c8297cfea042cb198029392f
nikhilnk8 <nikhilneo8@gmail.com>
2020-04-22 01:19:17 +0530

    3rd commit

--- Ecommerce/shop/views.py
+++ Ecommerce/shop/views.py
@@ -4,4 +4,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("we are at about")
+
+
+def contact(request):
+    return HttpResponse("we are at contact")
+
+
+def tracker(request):
+    return HttpResponse("we are at tracker")
+
+
+def search(request):
+    return HttpResponse("we are at search")
+
+
+def productView(request):
+    return HttpResponse("we are at product view")
+
+
+def checkout(request):
+    return HttpResponse("we are at checkout")
